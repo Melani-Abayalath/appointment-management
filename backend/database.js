4be4ed97f2995fdd6aec57a6087bc671159dadd5
Melani-Abayalath <155607702+Melani-Abayalath@users.noreply.github.com>
2025-10-03 14:49:37 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.CHOREO_DBCON1_DATABASENAME,
-  process.env.CHOREO_DBCON1_USERNAME,
-  process.env.CHOREO_DBCON1_PASSWORD,
+  process.env.CHOREO_XYZ_DATABASENAME,
+  process.env.CHOREO_XYZ_USERNAME,
+  process.env.CHOREO_XYZ_DATABASENAME,
   {
-    host: process.env.CHOREO_DBCON1_HOSTNAME,
+    host: process.env.CHOREO_XYZ_HOSTNAME,
     dialect: 'mysql',
-    port: process.env.CHOREO_DBCON1_PORT,
+    port: process.env.CHOREO_XYZ_PORT,
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false // This ensures the connection uses SSL without verifying the server's certificate
